a9ea4b1cd27a0c1b9a670a55ca62722bc203f9f5
kacpergorec <kacpergorec@gmail.com>
2022-12-31 14:35:04 +0100

    Second project, i had an issue described in stackoverflow.com/q/74962386. This commit has no issues so i can build project from here.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Menu\MenuBuilder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,34 @@
+<?php
+declare (strict_types=1);
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+
+class MenuBuilder
+{
+    public function __construct(
+        private FactoryInterface $factory,
+    )
+    {
+    }
+
+    public function createSideMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', ['route' => 'app_home']);
+
+//        foreach ($this->repository->findAll() as $page) {
+//            if ($page->isInMenu() && $page->isPublished()) {
+//                $menu->addChild($page->getTitle(), [
+//                    'route' => 'app_page',
+//                    'routeParameters' => ['slug' => $page->getSlug()]
+//                ]);
+//            }
+//        }
+
+        return $menu;
+    }
+}
