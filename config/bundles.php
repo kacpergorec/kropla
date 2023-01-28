fe2fa8c02320e837df50eb42ff427dad1383642d
kacpergorec <kacpergorec@gmail.com>
2023-01-28 15:23:00 +0100

    Config, templates ready for JWT

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
